a25a326a81bb80beab27b716c84bcdf0c247ebd0
2217wang <2677434110@qq.com>
2020-06-02 20:09:12 +0800

    修改话题详情页的样式，并且增加了回复下拉框
    回复的功能暂未实现，只是增加了一些互动的样式

--- src/components/wzy/topiccom.vue
+++ src/components/wzy/topiccom.vue
@@ -25,17 +25,65 @@
           <input type="submit" value="发表评论" v-on:click="sendcomment">
         </div>
         <div class="thecomment">
-          <div id="comment" v-for="num in comments" class="m-margin-top-big">
+          <div id="comment" v-for="(num, index) in commentParent" :key="num.id" :class="{'back':index%2 == 0}">
             <div class="commentleft">
-              <img src="../../../static/images/pea.png" v-on:click="tomypage(num.user_id)" />
+              <!-- <img src="../../../static/images/pea.png" v-on:click="tomypage(num.user_id)" /> -->
+              <el-avatar shape="circle" :size="medium" :src="circleUrl"></el-avatar>
             </div>
             <div class="commentright">
-              <div class="username m-margin-top-big">{{num.username}}</div>
-              <div class="usercontent">{{num.content}}</div>
-              <div class="metadata">
-                <span class="date">{{formatDate(new Date(num.create_time*1000))}}</span>
+              <div class="username">{{num.username}}</div>
+              <div class="usercontent">
+                &emsp;{{num.content}}
+                <span style="float: right;
+                  margin: 0 13px 0 13px;
+                  background-color: #CCCCCC;
+                  border-radius: 3px;" @click="getson(num.id, index)">
+                  查看回复
+                </span>
+                <span style="float: right;color:#AAAAAA;"
+                  @click="getson(num.id, index)">
+                  <i class="el-icon-date">{{formatDate(new Date(num.create_time*1000))}}</i>
+                </span>
               </div>
-              <div class="usercontent">回复</div>
+
+              <div v-if="num.showSon"
+                style="width: 100%;
+                background-color: #F0F8FF;
+                border: 1px solid;
+                padding: 5px;
+                overflow: hidden;">
+                <div class="thecomment" style="width: 100%;">
+                  <!-- 发表评论框 -->
+                  <div id="publish" style="width: 100%;">
+                    <textarea placeholder="输入你的评论.." v-model="userinput"></textarea>
+                    <input type="submit" value="发表评论" v-on:click="sendcomment">
+                  </div>
+                  <!-- 子评论循环体 -->
+                  <div class="comment" style="border: 1px solid; overflow: hidden;" v-for="item in commentParent">
+                    <div class="commentleft">
+                      <el-avatar size="small" :src="circleUrl"></el-avatar>
+                    </div>
+                    <div class="commentright">
+                      <div class="username" style="font-size: small;">
+                        <strong>{{item.username}}</strong>
+                      </div>
+                      <div class="usercontent">
+                        回复 <strong>{{num.username}}</strong> 说:&emsp;{{item.content}}
+                        <span style="float: right;color:#AAAAAA">
+                          <i class="el-icon-date">{{formatDate(new Date(item.create_time*1000))}}</i>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <!-- 发表评论框 -->
+                  <div id="publish" style="width: 100%;">
+                    <textarea placeholder="输入你的评论.." v-model="userinput"></textarea>
+                    <input type="submit" value="发表评论" v-on:click="sendcomment">
+                  </div>
+                </div>
+              </div>
+
+
             </div>
           </div>
         </div>
@@ -80,36 +128,56 @@
       return{
         topic:"话题内容",
         topic_id:3,//话题id，跳转页面时传入参数
-        comments:[],//存储服务器返回信息
+        comments:[],//存储服务器返回的评论信息
+        commentSon:[],
+        commentParent:[],
         message:'',//搜索框的输入信息
         userinput:'',//用户输入的评论内容
         user_id:window.sessionStorage.getItem('user_id'),//登录用户id
         total:1000,//分页总页数
-        commentdate:0,
+        showSon:false,
+        circleUrl: "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png",
       }
     },
     methods:{
+      updatedata(){
+        const _this=this
+        for(let c in _this.posts){
+          _this.posts[c].isShowComment=false
+        }
+      },
+
+      /**获取某评论的子评论
+       * @param {Object} id 父评论id
+       */
+      async getson(id, index) {
+        console.log(this.comments[index].showSon)
+        this.comments[index].showSon = !this.comments[index].showSon
+        console.log(this.comments[index].showSon)
+        // this.sonShow[index] = !this.sonShow[index]
+        // console.log(this.sonShow)
+        this.commentSon = []
+        for (let i=0; i<this.comments.length; i++){
+          if (this.comments[i].comment_id == id)
+            this.commentSon.push(this.comments[i])
+        }
+        await this.$forceUpdate();
+      },
+
+      /**计算评论的创建时间
+       * @param {Object} date
+       */
       formatDate(date) {
         var year=date.getFullYear();
         var month=date.getMonth()+1;
         var day=date.getDate();
-        var hour=date.getHours();
-        var minute=date.getMinutes();
-        var second=date.getSeconds();
-        this.commentdate = year + '-' +
-        (String(month).length > 1 ? month : '0' + month) + '-' +
-        (String(day).length > 1 ? day : '0' + day) + ' ' +
-        (String(hour).length > 1 ? hour : '0' + hour) + ':' +
-        (String(minute).length > 1 ? minute : '0' + minute) + ':' +
-        (String(second).length > 1 ? second : '0' + second)
-        // return
-        //   year + '-' +
-        //   (String(month).length > 1 ? month : '0' + month) + '-' +
-        //   (String(day).length > 1 ? day : '0' + day) + ' ' +
-        //   (String(hour).length > 1 ? hour : '0' + hour) + ':' +
-        //   (String(minute).length > 1 ? minute : '0' + minute) + ':' +
-        //   (String(second).length > 1 ? second : '0' + second)
+        return year + '-' + (String(month).length > 1 ? month : '0' + month) + '-' +
+          (String(day).length > 1 ? day : '0' + day)
       },
+
+      /**
+       * 返回全部话题页
+       */
       backtotopicpage(){
         this.$router.push("/topic")
       },
@@ -117,7 +185,9 @@
         alert(this.message)
       },
       tomypage:function(id){ },
-      //该函数是向服务器发送用户输入的评论
+      /**
+       * 向服务器发送用户输入的评论
+       */
       async sendcomment(){
         //获取topicid
         const _this=this
@@ -128,7 +198,6 @@
           this.topic_id=this.$route.path.split('/')[2]
           console.log('当前路径'+this.$route.path.split('/')[2])
         }
-        else{}
         const d = {
           content:this.userinput,
           user_id:window.sessionStorage.getItem('user_id') ,
@@ -147,10 +216,9 @@
           console.log("当前评论的用户id"+_this.user_id)
         })
         this.$router.go(0);
-        // location.reload();
       },
     },
-    created() {
+    async created() {
       this.topic_id=this.$route.path.split('/')[2]
       const d = {
         id:this.topic_id,
@@ -170,6 +238,11 @@
         _this.total=_this.comments.length
         console.log(_this.total)
         console.log("当前用户id为："+_this.user_id)
+        for (let c = 0; c<_this.comments.length; c++) {
+          if (_this.comments[c].comment_id === 0)
+            _this.commentParent.push(_this.comments[c])
+        }
+        console.log(_this.commentParent)
       })
       //获取topic_id对应的title
       this.$axios({
@@ -181,14 +254,19 @@
         _this.topic=r.data
         console.log(_this.topic)
       })
+      await _this.updatedata()
     }
   }
 </script>
 
 <style scoped>
+  .back {
+    background-color: #F2F2F2;
+  }
   .thecomment {
     background-color: white;
     width: 70%;
+    height: auto;
     margin: auto;
     margin-top: 3%;
     border-radius: 10px;
@@ -248,7 +326,7 @@
     box-sizing: border-box;
   }
   #publish textarea {
-    width: 80%;
+    width: 85%;
     height: 50px;
     padding: 1%;
     border-radius: 4px;
@@ -312,6 +390,7 @@
     width: 10%;
     float: left;
     padding: 1%;
+    margin: 0% 2% 0% 2%;
   }
   #comment .commentleft img {
     float: right;
