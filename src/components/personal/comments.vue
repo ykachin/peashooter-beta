e1a6c8fd216f46a3fab5cb7b7d38557795adfd9b
purity12138 <luan1343615717@126.com>
2020-06-03 15:56:13 +0800

    第一次签入

---
+++ src/components/personal/comments.vue
@@ -0,0 +1,127 @@
+<template>
+<div>
+  <el-breadcrumb separator-class="el-icon-arrow-right">
+    <el-breadcrumb-item :to="{ path: '/primarypage' }">首页</el-breadcrumb-item>
+    <el-breadcrumb-item>我的留言榜</el-breadcrumb-item>
+  </el-breadcrumb>
+  <div id="message" class="ui padded vertical segment m-padded-tb-large">
+    <!-- 留言显示 -->
+    <div id="message-title">我的最新留言</div>
+    <div id="message-content" class="m-margin-top">
+      <el-alert v-if="userMessageList.length === 0" title="ta暂时没有留言" type="info"></el-alert>
+      <div class="message-content-item" v-for="(item, index) in userMessageList" v-if="index < 5">
+        <el-input
+          style="margin-bottom: 10px;"
+          :value="item.content"
+          :disabled="true">
+        </el-input>
+        <span> from: {{ item.username }} </span>
+      </div>
+    </div>
+    <br>
+    <!-- 我的留言榜 -->
+    <div id="leave-message" class="m-margin-top">
+      <el-input
+        type="textarea"
+        placeholder="我想对自己说..."
+        v-model="messageContent"
+        maxlength="30"
+        show-word-limit
+      >
+      </el-input>
+      <br><br>
+      <el-button @click="leaveMessageToUser(myuserId, myuserId, messageContent)">留言</el-button>
+    </div>
+  </div>
+
+
+
+
+
+</div>
+
+</template>
+
+<script>
+  import axios from 'axios'
+  /*Vue.prototype.$axios = axios;*/
+  export default {
+    inject:['reload'],
+    data(){
+      return {
+        userMessageList:[],
+        messageContent:'',
+        myuserId:window.sessionStorage.getItem('user_id')
+      }
+    },
+    created(){
+      this.getUserMessageList(window.sessionStorage.getItem('user_id'))
+    },
+    methods:{
+      /**
+       * 获取自己留言列表
+       * @param userId  用户id
+       */
+      getUserMessageList(userId) {
+        var userid={id:userId}
+        const _this=this
+        this.$axios({
+          method: 'post',
+          url: '/api/user/getmessage',
+          data:_this.$qs.stringify(userid),
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        }).then(res => {
+          _this.userMessageList = res.data.data
+        }).catch(err => {
+          console.log(err)
+        })
+      },
+      /**
+       * 给自己留言
+       * @param fromUserId  留言来自的用户id
+       * @param toUserId  给谁留言
+       * @param content 内容
+       */
+      leaveMessageToUser(fromUserId, toUserId, content) {
+        const _this=this
+        var leaveMessage={from_user_id:fromUserId,to_user_id:toUserId,content:content}
+        console.log("留言内容：",leaveMessage)
+        this.$axios({
+          method: 'post',
+          url: '/api/messages',
+          data:_this.$qs.stringify(leaveMessage),
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        }).then(res => {
+          // 01. 提示留言成功
+          _this.$message({
+            message: '留言成功',
+            type: 'success'
+          })
+
+          // 02. 重新获取用户的留言列表
+          _this.getUserMessageList(toUserId)
+
+          // 03. 清空textarea的内容
+          _this.messageContent = ''
+        }).catch(err => {
+          _this.$message({
+            message: '留言失败',
+            type: 'error'
+          })
+          console.log("留言失败：",err)
+        })
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  #leave-message {
+    text-align: center;
+  }
+</style>
---
+++ src/components/personal/fans.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <el-card>
+      <el-row :gutter="20">
+        <el-col :span="10">
+          <el-input placeholder="请输入内容" v-model="input3" class="input-with-select">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary">删除</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
+
+</template>
+
+<script>
+    export default {
+      data(){
+        return{
+          id:5,
+        }
+      },
+      created(){
+        console.log("1")
+        this.getFansList()
+      },
+      methods:{
+        getFansList(){
+          console.log("2")
+          const _this=this
+          this.$axios({
+            method: 'post',
+            url: '/api/user/getfollow',
+            data:'id=5'
+          }).then(function (res) {
+            const r=res.data
+            _this.works=r.data
+            console.log(r.data)
+            /*console.log(res)
+            console.log('获取作品名',r.data[0].title)
+            console.log(11111111111)*/
+
+          }).catch(function (res) {
+            console.log("获取用户信息发生异常！请稍后重试...")
+          })
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
