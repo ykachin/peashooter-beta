7d53c137741bc3a0e58367b1cc0429354e7f7205
ququdefuqin <1665605828@qq.com>
2020-06-05 17:22:37 +0800

    解决图片服务器端不显示的问题
    把图片路径改成了相对路径

--- src/components/hrc/squareInfo.vue
+++ src/components/hrc/squareInfo.vue
@@ -73,7 +73,7 @@
                                     <div class="actions" >
                                       <a class="reply" data-commentid="1" data-commentnickname="Matt"  @click="reply(item.user_id,item.id,item.username)" >回复</a>
                                       <!--删除评论-->
-                                      <a @click="deletecomment(item.id)"><img  class="middle aligned" v-if="parseInt(item.user_id) === parseInt(currentuserid)" style="width: 18px" src="/static/images/delete.png"></a>
+                                      <a @click="deletecomment(item.id)"><img  class="middle aligned" v-if="parseInt(item.user_id) === parseInt(currentuserid)" style="width: 18px" src="../../../static/images/delete.png"></a>
                                     </div>
                                   </div>
                                   <div class="comments">
@@ -94,7 +94,7 @@
                                       <div class="actions" >
                                         <a class="reply" data-commentid="1" data-commentnickname="Matt"  @click="reply(item2.user_id,item.id)" >回复</a>
                                         <!--删除评论-->
-                                        <a @click="deletecomment(item2.id)"><img  class="middle aligned" v-if="parseInt(item2.user_id) === parseInt(currentuserid)" style="width: 18px" src="/static/images/delete.png"></a>
+                                        <a @click="deletecomment(item2.id)"><img  class="middle aligned" v-if="parseInt(item2.user_id) === parseInt(currentuserid)" style="width: 18px" src="../../../static/images/delete.png"></a>
                                       </div>
                                     </div>
                                   </div>
